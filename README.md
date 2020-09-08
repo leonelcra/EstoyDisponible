f444a5b0e514d93abfe51611c86c9bd504810238
Leonel Crainich <59263888+leonelcra@users.noreply.github.com>
2020-09-08 17:00:59 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# EstoyDisponible es una aplicación destinada a evitar el cambio de estado en MS Teams
+
+Mientras hago teletrabajo en cuarentena es normal que realice llamados telefónicos o anotaciones fuera de la computadora. Por tal motivo luego de unos pocos minutos el estado de MS Teams cambia a 'Ausente' aunque me encuentre al lado de mi computadora. Esto genera que mis colegas no me escriban pensando que no me encuentro disponible.
+Para evitar esta situación desarrollé en unos minutos esta aplicación destinada a evitar el cambio automático de estado.
+
+El código esta desarrollado en Visual Studio Community C# con Windows Forms
+
+Funciona de una manera muy sencilla. Simplemente ejecute el archivo [Teams en verde.exe](https://github.com/leonelcra/EstoyDisponible/blob/master/WF_mouse/bin/Release/Teams%20en%20verde.exe) en la carpeta wf_mouse/bin/Release/ descargado en su computadora y listo !
+
+Screenshot
+![Interface gráfica de la aplicación](EstoyDisponibleScreenshot.png)
+
+Mientras se encuentre ejecutandose la aplicación, el estado de MS Teams será Disponible y el protector de pantalla no se activará.
+
+Para volver al esta
+
---
+++ WF_mouse/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using teams_en_verde;
+
+namespace WF_mouse
+{
+    [FlagsAttribute]
+    public enum EXECUTION_STATE : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001
+    }
+
+    public partial class Form1 : Form
+    {
+        Keyboard keyboard; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Ejecutalo como admin...Teams en verde";
+            tmrMain.Interval = 60000;
+            tmrMain.Enabled = true;
+            keyboard = new Keyboard();
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        void PreventSleep()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+        }
+
+        public static void AllowSleep()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Esta acción volverá a permitir que Teams se vuelva a poner ausente luego de 5 minutos. Confirma?", "Cerrar aplicacón", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                AllowSleep();
+            }
+            else
+            {
+                e.Cancel = true;
+                this.Activate();
+            }
+        }
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            PreventSleep();
+            keyboard.Send(Keyboard.ScanCodeShort.OEM_1);
+        }
+    }
+}
